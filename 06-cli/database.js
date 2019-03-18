314ee52ad6621e7b44778dfb341332d7c7d9e5ca
rafameureer <rafameureer@gmail.com>
2019-03-18 13:09:45 -0300

    DELETE e CREATE

--- 06-cli/database.js
+++ 06-cli/database.js
@@ -1,8 +1,14 @@
-const { readFile } = require('fs');
+const {
+    readFile,
+    writeFile
+} = require('fs');
 
-const { promisify } = require('util');
+const {
+    promisify
+} = require('util');
 
 const readFileAsync = promisify(readFile);
+const writeFileAsync = promisify(writeFile);
 
 class Database {
     constructor() {
@@ -14,13 +20,42 @@ class Database {
         return JSON.parse(arquivo.toString());
     }
 
-    escreverArquivos() {
+    async cadastrar(heroi) {
+        const dados = await this.obterDadosArquivos();
+        const id = heroi.id <= 2 ? heroi.id : Date.now();
+        const heroiComId = {
+            id,
+            ...heroi
+        };
+        const dadosFinish = [
+            ...dados,
+            heroiComId
+        ];
+        const result = await this.escreverArquivos(dadosFinish);
+        return result;
+    }
 
+    async escreverArquivos(dados) {
+        await writeFileAsync(this.NOME_ARQUIVO, JSON.stringify(dados))
+        return true;
     }
 
     async listar(id) {
         const dados = await this.obterDadosArquivos();
         return dados.filter(item => (id ? item.id === id : true));
+    }
+
+    async remover(id) {
+        if (!id) {
+            return await this.escreverArquivos([]);
+        }
+        const dados = await this.obterDadosArquivos();
+        const index = dados.findIndex(item => item.id === parseInt(id));
+        if (index === -1) {
+            throw Error('O usuario informado não existe');
+        }
+        dados.splice(index, 1);
+        return await this.escreverArquivos(dados);
     }
 }
 
---
+++ 06-cli/test/test.js
@@ -0,0 +1,36 @@
+const {
+    deepEqual
+} = require('assert');
+
+const DEFAULT_ITEM_CADASTRAR = {
+    nome: 'Flash',
+    poder: 'Speed',
+    id: 1
+}
+
+const database = require('../database');
+
+describe('Suite de manipulação de Herois', () => {
+    before(async () => {
+        await database.cadastrar(DEFAULT_ITEM_CADASTRAR);
+    })
+    it('deve pesquisar um heroi usando arquivos', async () => {
+        const expected = DEFAULT_ITEM_CADASTRAR;
+        const [resultado] = await database.listar(expected.id);
+        deepEqual(resultado, expected);
+
+    });
+
+    it('deve cadastrar um heroi, usando arquivos', async () => {
+        const expected = DEFAULT_ITEM_CADASTRAR;
+        await database.cadastrar(DEFAULT_ITEM_CADASTRAR);
+        const [actual] = await database.listar(DEFAULT_ITEM_CADASTRAR.id);
+        deepEqual(actual, expected);
+    })
+
+    it('deve remover um heroi por id', async () => {
+        const expected = true;
+        const result = await database.remover(DEFAULT_ITEM_CADASTRAR.id);
+        deepEqual(result, expected);
+    });
+});
